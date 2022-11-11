ef9426030811760287e3b7df3f2bf9a4bd50e199
unknown <yousefsaber.155@gmail.com>
2022-11-11 15:09:08 +0200

    heu

--- BitOperations.py
+++ BitOperations.py
@@ -1,5 +1,11 @@
+import gmpy2
 from gmpy2 import xmpz
-
+FOUR_CONNECTED =  [4000,-4500]
+THREE_CONNECTED = [500,-500]
+TWO_CONNECTED = [100,-100]
+UN_CONNECTED=[500,-590]
+EMPTY = [50,-50]
+UNEMPTY = [-20,20]
 
 # from 0 to 41 is our board
 # from 42 to 63 each three bits represent column [6,7]
@@ -63,7 +69,7 @@ def bit_to_matrix(game):
         last_row = last_row.numerator
         a = 0|last_row
         for j in range(0, last_row):
-            print("i+j (" + str(j) + " " + str(i) + " )" + str(game[i * 6 + j]))
+            # print("i+j (" + str(j) + " " + str(i) + " )" + str(game[i * 6 + j]))
             # print(board[i][j])
             if game[i * 6 + j] == 0:
                 board[j][i] = 1
@@ -71,79 +77,336 @@ def bit_to_matrix(game):
                board[j][i] = 2
             # print(board[j][i])
     return board
-
-def heurastic():
+def max_horiz(game,i,j,visited):
+    if j==6:
+        visited[i][j][0]=True
+        return 1
+    if j+1>6:
+        return 0
+    if game[i][j]!=game[i][j+1]:
+        visited[i][j][0]=True
+        return 1
+    if game[i][j]==game[i][j+1]:
+        visited[i][j][0]=True
+        return max_horiz(game,i,j+1,visited)+1
+def max_vert(game,i,j,visited):
+    if i==5:
+        visited[i][j][1]=True
+        return 1
+    if i+1>5:
+        return 0
+    if game[i][j]!=game[i+1][j]:
+        visited[i][j][1]=True
+        return 1
+    if game[i][j]==game[i+1][j]:
+        visited[i][j][1]=True
+        return max_vert(game,i+1,j,visited)+1
+def max_p_d(game,i,j,visited):
+    if j==6 or i==5:
+        visited[i][j][2]=True
+        return 1
+    if j+1>6 or i+1>5:
+        return 0
+    if game[i][j]!=game[i+1][j+1]:
+        visited[i][j][2]=True
+        return 1
+    if game[i][j]==game[i+1][j+1]:
+        visited[i][j][2]=True
+        return max_p_d(game,i+1,j+1,visited)+1
+def max_n_d(game,i,j,visited):
+    if j==0 or i==5:
+        visited[i][j][3]=True
+        return 1
+    if j-1<0 or i+1>5:
+        return 0
+    if game[i][j]!=game[i+1][j-1]:
+        visited[i][j][3]=True
+        return 1
+    if game[i][j]==game[i+1][j-1]:
+        visited[i][j][3]=True
+        return max_p_d(game,i+1,j-1,visited)+1
+def un_connected_weighted_horz(board,i,j,visited,max_before,score):
+    if j+max_before<7 and board[i][j]!=board[i][j+max_before] and board[i][j+max_before]!=0:
+        if max_before!=1:
+            score+=(max_before/7)*UNEMPTY[board[i][j]-1]
+    else:
+        k= j+max_before
+        mx_h=max_before
+        while k<7 and  board[i][k]==0:
+            if(mx_h!=1):
+                score+=(mx_h/7)*EMPTY[board[i][j]-1]
+            if k+1>=7 or board[i][j]!=board[i][k+1]:
+                break
+            if k+1<7 and board[i][j]==board[i][k+1]:
+                mx_h=max_horiz(board,i,k+1,visited)
+                if mx_h == 1:
+                    if board[i][j]==1:
+                        score+=5
+                    else:
+                        score-=10
+                elif mx_h==2:
+                    score+=TWO_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(1/4.0)
+                elif mx_h==3:
+                    score+=THREE_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(2/4.0)
+                elif mx_h==4:
+                    score+=FOUR_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(3/4.0)
+                k=k+mx_h+1
+            if k<7 and board[i][k]!=board[i][j] and board[i][k]!=0:
+                if mx_h!=1:
+                    score+=(mx_h/7)*UNEMPTY[board[i][j]-1]
     return score
-# game[0:3] = 7|game[42:62]
-game = xmpz(0)
-# game[0:63]=~0
-# game=game.bit_clear(42)
-# game=game.bit_clear(45)
-# game=game.bit_clear(48)
-# game=game.bit_clear(51)
-# game=game.bit_clear(54)
-# game=game.bit_clear(57)
-# game=game.bit_clear(60)
-print((game[42:63]))
-# # game[45:48]=~0
-# print(bin(game))
-# print(game.bit_length())
-state, game = play(0, game, 1)
-state, game = play(0, game, 1)
-state, game = play(0, game, 1)
-state, game = play(0, game, 1)
-state, game = play(0, game, 1)
-state, game = play(0, game, 1)
-# print(game,state)
-state, game = play(1, game, 1)
-state, game = play(1, game, 1)
-state, game = play(1, game, 1)
-state, game = play(1, game, 1)
-state, game = play(1, game, 1)
-state, game = play(1, game, 1)
-state, game = play(1, game, 1)
-# print(state)
-state, game = play(2, game, 1)
-state, game = play(2, game, 1)
-state, game = play(2, game, 1)
-state, game = play(2, game, 1)
-state, game = play(2, game, 1)
-state, game = play(2, game, 1)
-state, game = play(2, game, 1)
-# print(state)
-state, game = play(3, game, 1)
-state, game = play(3, game, 1)
-state, game = play(3, game, 1)
-state, game = play(3, game, 1)
-state, game = play(3, game, 1)
-state, game = play(3, game, 1)
-state, game = play(3, game, 1)
-state, game = play(4, game, 1)
-state, game = play(4, game, 1)
-state, game = play(4, game, 1)
-state, game = play(4, game, 1)
-state, game = play(4, game, 1)
-state, game = play(4, game, 0)
-# state,game= play(4,game,0)
-state, game = play(5, game, 1)
-state, game = play(5, game, 1)
-state, game = play(5, game, 1)
-state, game = play(5, game, 1)
-state, game = play(5, game, 1)
-state, game = play(5, game, 1)
-state, game = play(5, game, 1)
-state, game = play(6, game, 1)
-state, game = play(6, game, 1)
-state, game = play(6, game, 1)
-state, game = play(6, game, 1)
-state, game = play(6, game, 1)
-state, game = play(6, game, 1)
-state, game = play(6, game, 1)
-state, game = play(6, game, 1)
-data = bit_to_matrix(game=game)
-# print(bit_to_matrix(game=xmpz(0)))
-for row in data:
-    print(row)
+def un_connected_d_p(board,i,j,visited,max_before,score):
+    if i+max_before<6 and j+max_before<7 and board[i][j]!=board[i+max_before][j] and board[i+max_before][j+max_before]!=0:
+        if max_before!=1:
+            score+=(max_before/6)*UNEMPTY[board[i][j]-1]
+    else:
+        new_i= i+max_before
+        new_j= j+max_before
+        mx_h=max_before
+        while new_i<6 and new_j<7 and  board[new_i][new_j]==0:
+            if mx_h!=1:
+                score+=(mx_h/6)*EMPTY[board[i][j]-1]
+            if new_i+1>=6 or new_j+1>=7 or board[i][j]!=board[new_i+1][new_j+1]:
+                break
+            if new_i+1<6 and new_j+1<7 and board[i][j]==board[new_i+1][new_j+1]:
+                mx_h=max_p_d(board,new_i+1,new_j+1,visited)
+                if mx_h == 1:
+                    if board[i][j]==1:
+                        score+=5
+                    else:
+                        score-=10
+                elif mx_h==2:
+                    score+=TWO_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(1/4.0)
+                elif mx_h==3:
+                    score+=THREE_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(2/4.0)
+                elif mx_h==4:
+                    score+=FOUR_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(3/4.0)
+                new_i=new_i+mx_h+1
+                new_j=new_j+mx_h+1
+            if new_i<6 and new_j<7 and board[new_i][new_j]!=board[i][j] and board[new_i][new_j]!=0 :
+                if(mx_h!=1):
+                    score+=(mx_h/6)*UNEMPTY[board[i][j]-1]
+    return score
+def un_connected_d_n(board,i,j,visited,max_before,score):
+    if i+max_before<6 and j-max_before>-1 and board[i][j]!=board[i+max_before][j-max_before] and board[i+max_before][j-max_before]!=0:
+        if max_before!=1:
+            score+=(max_before/6)*UNEMPTY[board[i][j]-1]
+    else:
+        new_i= i+max_before
+        new_j= j-max_before
+        mx_h=max_before
+        while new_i<6 and new_j>-1 and  board[new_i][new_j]==0:
+            if mx_h!=1:
+                score+=(mx_h/6)*EMPTY[board[i][j]-1]
+            if new_i+1>=6 or new_j-1<0 or board[i][j]!=board[new_i+1][new_j-1]:
+                break
+            if new_i+1<6 and new_j-1>-1 and board[i][j]==board[new_i+1][new_j-1]:
+                mx_h=max_n_d(board,new_i+1,new_j-1,visited)
+                if mx_h == 1:
+                    if board[i][j]==1:
+                        score+=5
+                    else:
+                        score-=10
+                elif mx_h==2:
+                    score+=TWO_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(1/4.0)
+                elif mx_h==3:
+                    score+=THREE_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(2/4.0)
+                elif mx_h==4:
+                    score+=FOUR_CONNECTED[board[i][j]-1]
+                    score+=UN_CONNECTED[board[i][j]-1]*(3/4.0)
+                # k=k+mx_h+1
+                new_i=new_i+mx_h+1
+                new_j=new_j-mx_h-1
+            if new_i<6 and new_j>-1 and board[new_i][new_j]!=board[i][j] and board[new_i][new_j]!=0 :
+                if mx_h!=1:
+                    score+=(mx_h/6)*UNEMPTY[board[i][j]-1]
+    return score
+def heurastic1(board):
+    # board=bit_to_matrix(game)
+    """
+    visited is 3d array to reduce reduandancy
+    every intial element responsible for direct 0 horiz 1 ver 2 postive diagonal 
+    3 negative diagonal
+    """
+    visited= [[[False for k in range(4)] for j in range(7)] for i in range(6)] 
+    score=0
+    for i in range(0,6):
+        for j in range(0,7):
+            if board[i][j]==0:
+                continue
+            if(not visited[i][j][0]):
+                mx=max_horiz(board,i,j,visited)
+                if mx>=4:
+                    score+=FOUR_CONNECTED[board[i][j]-1]*(mx-3)
+                    k= j+mx
+                    if k<7:
+                        if board[k][j]==0:
+                            score+=EMPTY[board[i][j]-1]
+                        if k+1<7 and board[i][j]==board[i][k+1]:
+                            max_h=max_horiz(board,i,k+1,visited)
+                            if max_h==2:
+                                score+=TWO_CONNECTED[board[i][j]-1]
+                                score+=UN_CONNECTED[board[i][j]-1]*(1/4.0)
+                            if max_h==1:
+                                if board[i][j]==1:
+                                    score+=5
+                                else:
+                                    score-=10
+                elif mx==3:
+                    score+=THREE_CONNECTED[board[i][j]-1]
+                    score=un_connected_weighted_horz(board,i,j,visited,mx,score)
+                elif mx==2:
+                    score+=TWO_CONNECTED[board[i][j]-1]
+                    score=un_connected_weighted_horz(board,i,j,visited,mx,score)
+                elif mx==1:
+                    score=un_connected_weighted_horz(board,i,j,visited,mx,score)
+            if(not visited[i][j][1]):
+                max=max_vert(board,i,j,visited)
+                if max>=4:
+                    score+=FOUR_CONNECTED[board[i][j]-1]*(max-3)
+                elif max==3:
+                    score+=THREE_CONNECTED[board[i][j]-1]
+                    if i+max<6 and board[i][j]!=board[i+max][j] and board[i+max][j]!=0:
+                        score+=UNEMPTY[board[i][j]-1]
+                elif max==2:
+                    score+=TWO_CONNECTED[board[i][j]-1]
+            if(not visited[i][j][2]):
+                max=max_p_d(board,i,j,visited)
+                if max>=4:
+                    score+=FOUR_CONNECTED[board[i][j]-1]*(max-3)
+                    new_i= i+max
+                    new_j=j+max
+                    if new_i<6 and new_j<7 and board[new_i][new_j]==0:
+                        score+=EMPTY[board[i][j]-1]
+                        if max==4:
+                            if new_i+1<6 and new_j+1<7 and board[i][j]==board[new_i+1][new_j+1]:
+                                if board[i][j]==1:
+                                    score+=5
+                                else:
+                                    score-=10 
+                elif max==3:
+                    score+=THREE_CONNECTED[board[i][j]-1]
+                    score=un_connected_d_p(board,i,j,visited,max,score)
+                elif max==2:
+                    score+=TWO_CONNECTED[board[i][j]-1]
+                    score=un_connected_d_p(board,i,j,visited,max,score)
+                elif max==1:
+                    score=un_connected_d_p(board,i,j,visited,max,score)
+            if(not visited[i][j][3]):
+                max=max_n_d(board,i,j,visited)
+                if max>=4:
+                    score+=FOUR_CONNECTED[board[i][j]-1]*(max-3)
+                    new_i= i+max
+                    new_j=j-max
+                    if new_i<6 and new_j>-1 and board[new_i][new_j]==0:
+                        score+=EMPTY[board[i][j]-1]
+                        if max==4:
+                            if new_i+1<6 and new_j+1<7 and board[i][j]==board[new_i+1][new_j+1]:
+                                if board[i][j]==1:
+                                    score+=5
+                                else:
+                                    score-=10 
+                elif max==3:
+                    score+=THREE_CONNECTED[board[i][j]-1]
+                    score=un_connected_d_n(board,i,j,visited,max,score)
+                elif max==2:
+                    score+=TWO_CONNECTED[board[i][j]-1]
+                    score=un_connected_d_n(board,i,j,visited,max,score)
+                elif max==1:
+                    score=un_connected_d_n(board,i,j,visited,max,score)
+    PRIORITY=3
+    for i in range(0,2):
+        for j in range(0,7):
+            if board[i][j]==1:
+                score+=PRIORITY
+            elif board[i][j]==2:
+                score-=PRIORITY
+    for i in range(0,6):
+        for j in range(0,3):
+            if board[i][j]==1:
+                score+=PRIORITY
+            elif board[i][j]==2:
+                score-=PRIORITY
+    return score
+# # game[0:3] = 7|game[42:62]
+# game = xmpz(0)
+# # game[0:63]=~0
+# # game=game.bit_clear(42)
+# # game=game.bit_clear(45)
+# # game=game.bit_clear(48)
+# # game=game.bit_clear(51)
+# # game=game.bit_clear(54)
+# # game=game.bit_clear(57)
+# # game=game.bit_clear(60)
+# print((game[42:63]))
+# # # game[45:48]=~0
+# # print(bin(game))
+# # print(game.bit_length())
+# state, game = play(0, game, 1)
+# state, game = play(0, game, 1)
+# state, game = play(0, game, 1)
+# state, game = play(0, game, 1)
+# state, game = play(0, game, 1)
+# state, game = play(0, game, 1)
+# # print(game,state)
+# state, game = play(1, game, 1)
+# state, game = play(1, game, 1)
+# state, game = play(1, game, 1)
+# state, game = play(1, game, 1)
+# state, game = play(1, game, 1)
+# state, game = play(1, game, 1)
+# state, game = play(1, game, 1)
+# # print(state)
+# state, game = play(2, game, 1)
+# state, game = play(2, game, 1)
+# state, game = play(2, game, 1)
+# state, game = play(2, game, 1)
+# state, game = play(2, game, 1)
+# state, game = play(2, game, 1)
+# state, game = play(2, game, 1)
+# # print(state)
+# state, game = play(3, game, 1)
+# state, game = play(3, game, 1)
+# state, game = play(3, game, 1)
+# state, game = play(3, game, 1)
+# state, game = play(3, game, 1)
+# state, game = play(3, game, 1)
+# state, game = play(3, game, 1)
+# state, game = play(4, game, 1)
+# state, game = play(4, game, 1)
+# state, game = play(4, game, 1)
+# state, game = play(4, game, 1)
+# state, game = play(4, game, 1)
+# state, game = play(4, game, 0)
+# # state,game= play(4,game,0)
+# state, game = play(5, game, 1)
+# state, game = play(5, game, 1)
+# state, game = play(5, game, 1)
+# state, game = play(5, game, 1)
+# state, game = play(5, game, 1)
+# state, game = play(5, game, 1)
+# state, game = play(5, game, 1)
+# state, game = play(6, game, 1)
+# state, game = play(6, game, 1)
+# state, game = play(6, game, 1)
+# state, game = play(6, game, 1)
+# state, game = play(6, game, 1)
+# state, game = play(6, game, 1)
+# state, game = play(6, game, 1)
+# state, game = play(6, game, 1)
+
+# data = bit_to_matrix(game=game)
+# # print(bit_to_matrix(game=xmpz(0)))
+# for row in data:
+    # print(row)
 def four_horz(board,i,j):
     if board[i][j]==board[i][j+1] and board[i][j]==board[i][j+2] and board[i][j]==board[i][j+3]:
         return 1 
@@ -174,7 +437,7 @@ def calculate_final_score(board):
                 """
                 in the end of game it must all places are full 
                 """
-                return None
+                continue
             if board[i][j]==1:
                 if i+3<6:
                     player1_sc+=four_vert(board,i,j)
@@ -202,5 +465,25 @@ def calculate_final_score(board):
 #     [1,2,1,2,1,1,2],
 #     [1,1,1,1,1,1,2],
 # ]
-a,b=calculate_final_score(board)
-print(a,b)
+# a,b=calculate_final_score(board)
+# print(a,b)
+state, game = play(0, xmpz(0), 1)
+state, game = play(0, game, 1)
+state, game = play(0, game, 1)
+state, game = play(0, game, 1)
+state, game = play(0, game, 0)
+state, game = play(6, game, 0)
+state, t = play(0, game, 1)
+print(bin(t))
+board = [
+    [1,1,1,0,1,1,1],
+    [2,2,2,0,2,2,2],
+    [0,0,0,0,0,0,0],
+    [1,1,1,0,2,2,2],
+    [0,0,0,0,0,0,0],
+    [0,0,0,1,1,2,2],
+]
+sc=heurastic1(board)
+# visited= [[[False for k in range(4)] for j in range(7)] for i in range(6)] 
+# sc=max_vert(board,0,0,visited)
+print(sc)
