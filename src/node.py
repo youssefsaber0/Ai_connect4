68c2ee83d69d3778ace08ac15f6a0f1d9fb1f1d9
Zeyad Zidan <81028196+iamzeyadzidan@users.noreply.github.com>
2022-11-12 15:27:43 +0200

    Modified node to save its action.

--- src/node.py
+++ src/node.py
@@ -4,8 +4,9 @@ This file represents the node class.
 
 
 class Node:
-    def __init__(self, children, turn, state, value=None):
+    def __init__(self, children, turn, state, action=None, value=None):
         self.children = children
         self.turn = turn
         self.state = state
+        self.action = action
         self.value = value
