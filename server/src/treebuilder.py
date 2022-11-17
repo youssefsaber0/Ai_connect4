a0d94a5c8dd75247d008e279602549f3df67a32d
Zeyad Zidan <81028196+iamzeyadzidan@users.noreply.github.com>
2022-11-17 22:05:11 +0200

    Final modifications.

--- server/src/treebuilder.py
+++ server/src/treebuilder.py
@@ -1,17 +1,21 @@
 """
-Constructs a tree in an in-order ordering.
+Constructs a tree in a pre-order motive.
 Arguments:
     root: the root node of the tree.
 Returns:
     tree representation.
 """
 
-tree = []   # This array contains the tree nodes in an in-order way.
-
 
 def construct_tree(root):
+    tree = []
+    preorder(tree, root)
+    return tree
+
+
+def preorder(tree, root):
     if root is None:
         return
-    for i in range(len(root.children)):
-        construct_tree(root.children[i])
     tree.append(root)
+    for i in range(len(root.children)):
+        preorder(tree, root.children[i])
